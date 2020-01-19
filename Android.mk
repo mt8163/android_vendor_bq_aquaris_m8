3384b2891cb61f149481ab723efea29e3fa11d7a
Roger Ortiz <rogerortizleal@gmail.com>
2020-01-19 15:38:54 +0100

    vendor: Test for lineage 14.1

--- Android.mk
+++ Android.mk
@@ -17,29 +17,46 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),aquaris_m8)
-
-$(info [aquaris_m8] Including vendor blobs....)
+ifeq ($(TARGET_DEVICE),bq)
+$(info [Aquaris M8] Copying DP Framework proprietary blobs)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE = libcam.utils.sensorlistener
+LOCAL_MODULE = libaudiocustparam
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_OWNER = $VENDOR
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MULTILIB = 32
+LOCAL_SRC_FILES_32 := proprietary/lib/libaudiocustparam.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE = libaudiocustparam
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_OWNER = $VENDOR
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MULTILIB = 64
+LOCAL_SRC_FILES_64 := proprietary/lib64/libaudiocustparam.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_OWNER = $VENDOR
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_PROPRIETARY_MODULE = true
+LOCAL_MULTILIB = 64
+LOCAL_SRC_FILES_64 = proprietary/lib64/libdpframework.so
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
 LOCAL_MODULE_CLASS = SHARED_LIBRARIES
 LOCAL_MODULE_OWNER = mtk
 LOCAL_MODULE_SUFFIX = .so
 LOCAL_PROPRIETARY_MODULE = true
-LOCAL_MULTILIB := both
-LOCAL_SRC_FILES_32 = proprietary/system/lib/libcam.utils.sensorlistener.so
-LOCAL_SRC_FILES_64 = proprietary/system/lib64/libcam.utils.sensorlistener.so
+LOCAL_MULTILIB = 32
+LOCAL_SRC_FILES_32 = proprietary/lib/libdpframework.so
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libion_mtk
-LOCAL_SRC_FILES_32 := proprietary/system/lib/libion_mtk.so
-LOCAL_SRC_FILES_64 := proprietary/system/lib64/libion_mtk.so
-LOCAL_PROPRIETARY_MODULE = true
-LOCAL_MULTILIB := both
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
-
 endif
