4ba7533bfe3f77ffd132a776a850df9ccb34f06b
R0rt1z2 <rogerortizleal@gmail.com>
2020-04-03 00:13:36 +0200

    aquaris_m8: Add gui libraries again.
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- aquaris_m8-vendor.mk
+++ aquaris_m8-vendor.mk
@@ -231,6 +231,9 @@ PRODUCT_COPY_FILES += \
  vendor/bq/aquaris_m8/proprietary/lib64/libzteevdo-ril.so:system/lib64/libzteevdo-ril.so \
  vendor/bq/aquaris_m8/proprietary/lib64/libzteevdo-ril.so:system/lib64/libzteevdo-ril.so \
  vendor/bq/aquaris_m8/proprietary/lib64/lib_uree_mtk_video_secure_al.so:system/lib64/lib_uree_mtk_video_secure_al.so \
+ vendor/bq/aquaris_m8/proprietary/lib64/libgui_ext.so:system/lib64/libgui_ext.so \
+ vendor/bq/aquaris_m8/proprietary/lib64/libui_ext.so:system/lib64/libui_ext.so \
+ vendor/bq/aquaris_m8/proprietary/lib64/libperfservicenative.so:system/lib64/libperfservicenative.so \
  vendor/bq/aquaris_m8/proprietary/lib64/hw/audio.a2dp.default.so:system/lib64/hw/audio.a2dp.default.so \
  vendor/bq/aquaris_m8/proprietary/lib64/hw/audio_policy.stub.so:system/lib64/hw/audio_policy.stub.so \
  vendor/bq/aquaris_m8/proprietary/lib64/hw/camera.mt8163.so:system/lib64/hw/camera.mt8163.so \
@@ -433,6 +436,9 @@ PRODUCT_COPY_FILES += \
  vendor/bq/aquaris_m8/proprietary/lib/libzteevdo-ril.so:system/lib/libzteevdo-ril.so \
  vendor/bq/aquaris_m8/proprietary/lib/zterilPara:system/lib/zterilPara \
  vendor/bq/aquaris_m8/proprietary/lib/lib_uree_mtk_video_secure_al.so:system/lib/lib_uree_mtk_video_secure_al.so \
+ vendor/bq/aquaris_m8/proprietary/lib/libgui_ext.so:system/lib64/libgui_ext.so \
+ vendor/bq/aquaris_m8/proprietary/lib/libui_ext.so:system/lib64/libui_ext.so \
+ vendor/bq/aquaris_m8/proprietary/lib/libperfservicenative.so:system/lib64/libperfservicenative.so \
  vendor/bq/aquaris_m8/proprietary/lib/soundfx/libaudiopreprocessing.so:system/lib/soundfx/libaudiopreprocessing.so \
  vendor/bq/aquaris_m8/proprietary/lib/hw/audio.a2dp.default.so:system/lib/hw/audio.a2dp.default.so \
  vendor/bq/aquaris_m8/proprietary/lib/hw/audio_policy.stub.so:system/lib/hw/audio_policy.stub.so \
