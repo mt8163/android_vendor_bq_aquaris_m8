b30b52461583e2f58db44d7f7607ab36edcf831a
R0rt1z2 <rogerortizleal@gmail.com>
2020-04-08 13:21:08 +0200

    aquaris_m8: Improve the bluetooth manager.
    Add option to enable bluetooth back (Testing).
    
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- proprietary/bin/bt_disable.sh
+++
@@ -1,8 +0,0 @@
-#!/system/bin/sh
-
-if [ -f "/system/app/Bluetooth/Bluetooth.apk" ]; then
-   log "Bye Bye Bluetooth"
-   mount -o rw,remount /system
-   rm -rf /system/app/Bluetooth/Bluetooth.apk
-   mount -o ro,remount /system
-fi
